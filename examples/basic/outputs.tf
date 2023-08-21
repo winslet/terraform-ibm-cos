60b613ae7f3da356af7f6bf9eb78b742d1551f51
Sam Winslet <samwinslet@gmail.com>
2023-08-21 17:52:14 -0500

    fix-remove-submodule

--- examples/basic/outputs.tf
+++ examples/basic/outputs.tf
@@ -12,8 +12,3 @@ output "bucket_id" {
   description = "Bucket id"
   value       = module.cos.bucket_id
 }
-
-output "buckets" {
-  description = "Bucket from sub module"
-  value       = module.buckets.buckets
-}
